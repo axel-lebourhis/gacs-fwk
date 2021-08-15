c34ba3cde872fe192bfa34ee1ca6feac3468dc78
Axel Le Bourhis <lebourhis.axel@hotmail.fr>
2021-08-16 00:16:23 +0200

    Client to keep the connection alive
    Signed-off-by: Axel Le Bourhis <lebourhis.axel@hotmail.fr>

--- tests/test_gacs_client.cpp
+++ tests/test_gacs_client.cpp
@@ -19,5 +19,47 @@ int main(void)
 
     client.connect("127.0.0.1", 60000);
 
+    bool exit = false;
+    while(!exit)
+    {
+        if(client.is_connected())
+        {
+            if(!client.incoming().empty())
+            {
+                auto msg = client.incoming().pop_front().msg;
+
+                switch (msg.header.id)
+                {
+                case MessageTypes::ServerAccept:
+                    std::cout << "Server Accepted Connection\n";
+                    break;
+
+                case MessageTypes::ServerDeny:
+                    std::cout << "Connection denied by server\n";
+                    exit = true;
+                    break;
+
+                case MessageTypes::ServerPing:
+                    std::cout << "Ping from server\n";
+                    break;
+
+                case MessageTypes::ServerMessage:
+                    break;
+
+                default:
+                    std::cout << "Unknown message\n";
+                    break;
+                }
+            }
+        }
+        else
+        {
+            std::cout << "Server is down\n";
+            exit = true;
+        }
+    }
+
+    // system("pause");
+
     return 0;
 }
