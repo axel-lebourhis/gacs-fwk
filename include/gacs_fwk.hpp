ff9be890c8b5d9d21b0f6f75f7cf9b25facea927
Axel Le Bourhis <lebourhis.axel@hotmail.fr>
2022-06-07 23:11:07 +0200

    Add clang-format exclusion for include order
    Signed-off-by: Axel Le Bourhis <lebourhis.axel@hotmail.fr>

--- include/gacs_fwk.hpp
+++ include/gacs_fwk.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
-#include "gacs_client.hpp"
+// clang-format off
 #include "gacs_common.hpp"
+#include "gacs_server.hpp"
+#include "gacs_client.hpp"
+#include "gacs_tsqueue.hpp"
 #include "gacs_connection.hpp"
 #include "gacs_message.hpp"
-#include "gacs_server.hpp"
-#include "gacs_tsqueue.hpp"
+// clang-format on
